fe6bfef4bc68af948fab050a7c7dd036d733280c
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-06-04 16:58:05 -0500

    Created model for resturants

--- thebreeze_app/models.py
+++ thebreeze_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Resturant(models.Model):
+    name = models.CharField(max_length=200, blank=False, unique=True)
+    direction = models.CharField(max_length=200, blank=False, unique=True)
+    hyper_link = models.CharField(max_length=500, blank=False, unique=True)
+    hours_open = models.CharField(max_length=200, blank=False, unique=True)
+    description = models.CharField(max_length=200, blank=False, unique=True)
+
+    def __str__(self):
+        return f'Name: {self.name} /n Direction: {self.direction} /n {self.hyper_link} /n Hours: {self.hours_open} /n Description: {self.description}'
