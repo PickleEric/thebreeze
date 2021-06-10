b9930b3023551e3a7e7088659ed09c07b5cc64ad
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-06-09 23:21:06 -0500

    added login and logout redirects to settings.py

--- thebreeze/settings.py
+++ thebreeze/settings.py
@@ -114,7 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGIN_REDIRECT_URL = 'my_user_profile'
+LOGOUT_REDIRECT_URL = 'goodbye'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
